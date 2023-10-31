5994579c6d444270c42ab127a5bdca6e7586556c
hxclover <rbecca.lam@gmail.com>
2023-10-30 17:13:42 -0700

    Completed

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 // ******* STATE *******
-const state = {};
+const state = {{ name: "apple", quantity: 6. price: 1.75 },
+{ name: "banana", quantity:6000, price: 0.25 },};
 
 const items = [
   { name: "apple", price: 1.75 },
@@ -17,9 +18,52 @@ const items = [
 ];
 
 // ******* REFERENCES *******
-
+const form = document.querySelector("form");
+const itemInput = document.querySelector("#grocery-input");
+const addButton = document.querySelector(".submit-btn");
+const clearButton = document.getElementsByClassName("clear-btn");
+const tableBody = document.c
 // ******* EVENT LISTENERS *******
+addButton.addEventListener("click", (event) => {
+  event.preventDefault();
+  const name = itemInput.value;
+  const quantity = Math.floor(Math.random() * 10);
+  const price = Math.floor(Math.random() * 50);
+
+  state.groceries.push({name, quantity, price});
+
+  itemInput.value = "";
+
+  console.log("groceries: ", state.groceries);
+
+});
+// Initial rendering
+
+render();
+
+
+clearButton.addEventListener("click", () =>{
+  state.groceries = state.groceries = [];
+  console.log
+})
 
 // ******* RENDER *******
+function render() {
+  state.groceries.map((item) => {
+    const newRow = document.createElement("tr");
+    
 
+    const name = document.createElement("td")
+    name.innerText = item.name;
+    
+    const quantity = document.createElement("td")
+    quantity.innerText = item.quantity;
+    
+    const price = document.createElement("td")
+    price.innerText = item.price;
+    
+    console.log("new row: "newRow);
+    return newRow;
+  });
+}
 // ******* FUNCTIONS *******
